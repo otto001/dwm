b3f19a5878383197ad8747924d7205831f63cb02
otto001 <matteo.ludwig@gmail.com>
2021-01-29 16:17:51 +0100

    dmenu_run_sudo

--- config.h
+++ config.h
@@ -57,7 +57,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-F", "-i", NULL };
+static const char *dmenucmdsudo[] = { "dmenu_run_sudo", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-F", "-i", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static const char *upvol[]   = { "dwm_vol", "-cmd", "up", "-m", dmenumon, "-fn", dmenufont, "-b", col_gray1, "-sb", col_cyan, NULL };
@@ -76,7 +77,8 @@ static Key keys[] = {
     { MODKEY,                       XK_s,                    spawn,          {.v = interactive } },
 
         //predefined
-	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
+    { MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
+    { MODKEY|ShiftMask,             XK_space,  spawn,          {.v = dmenucmdsudo } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
